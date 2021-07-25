d5e606bf5f4268ad313b4726e5b8480844df2fcf
Victoria Androsenko <androsenko.victoria@gmail.com>
2021-07-25 19:00:00 +0400

    completed first task

---
+++ src/ru/isys/trainings/homework5/Computer.java
@@ -0,0 +1,26 @@
+package ru.isys.trainings.homework5;
+
+public class Computer {
+    private ComputerInfo computerInfo;
+
+    public Computer(ComputerInfo computerInfo) {
+        this.computerInfo = computerInfo;
+    }
+
+    public ComputerInfo getComputerInfo() {
+        return computerInfo;
+    }
+
+    private class ComputerInfo {
+        private String os;
+        private String cpu;
+        private String ram;
+
+        public ComputerInfo(String os, String cpu, String ram) {
+            this.os = os;
+            this.cpu = cpu;
+            this.ram = ram;
+        }
+
+    }
+}
